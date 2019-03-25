4a45592d16cfe625a183e57db103ab15f3291fad
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-25 18:40:47 +0530

    Commit

--- js/apicall.js
+++ js/apicall.js
@@ -1,6 +1,6 @@
 import instance from './news';
 
-// instance.SingletonOperation();
+instance.SingletonOperation();
 
-// const url = "https://newsapi.org/v1/articles?source=bbc-news&apiKey=720c0314e8b2423eb7e1ffca5a1eeeb1";
+ const url = "https://newsapi.org/v1/articles?source=bbc-news&apiKey=720c0314e8b2423eb7e1ffca5a1eeeb1";
  
