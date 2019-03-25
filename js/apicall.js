523811e45dfbf64ab42d2059e48f0632f79a0e12
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-25 17:25:14 +0530

    Commit

--- js/apicall.js
+++ js/apicall.js
@@ -2,13 +2,7 @@
 let _singleton = null;
 class fetchdata {
     constructor (data) {
-        if(!_singleton) {
-            this.data = data
-            _singleton = this
-        }
-        else
-            return _singleton
-        
+
     }
 
     SingletonOperation () {
