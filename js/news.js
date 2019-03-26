17b90ea482cabf1502a369639b5396f91b5791eb
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-26 15:47:32 +0530

    Commit

--- js/news.js
+++ js/news.js
@@ -1,40 +1,34 @@
 
 'use strict';
 let _singleton = null;
-let globalData ={};
+
 class fetchdata {
-    constructor (data) {
-        
-    }
-    SingletonOperation (url) {
-        fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(myJson => {
+  constructor(data) {
+  }
+  SingletonOperation(url) {
+    let globalData = {};
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
         globalData = myJson;
         let elementId = document.getElementById('newsDetails');
-        let returnHtml = "";
-        let duplicateResolve = "";
-        let uniqueVal =[];
-        let storeAuthor=""
-         globalData.articles.map((value, index) =>{
-           uniqueVal = `<h1>${value.author}</h1>`
-           if(index > 0){
-                uniqueVal = "";
-           }
+        let returnHtml = "",
+          uniqueVal = [];
+
+        globalData.articles.map((value, index) => {
+          uniqueVal = `<h1>${value.author}</h1>`
+          if (index > 0) {
+            uniqueVal = "";
+          }
           returnHtml += `${uniqueVal}<div class="newsTitle">${value.title}</div><div class="newsDescription">${value.description}</div><div class="publishDate">${value.publishedAt}</div><div class="imageContainer"><a href="${value.url}" target="_blank"><img src=${value.urlToImage} /></a></div>`;
         });
         console.log(returnHtml);
         elementId.insertAdjacentHTML('afterbegin', returnHtml);
-        })
-        .catch(error => {console.log("Something went wrong on server")});
-    }
-    // GetSingletonData () {
-        
-    // }
+      })
+      .catch(error => { console.log("Something went wrong on server") });
+  }
 }
-
 const instance = new fetchdata();
-
 export default instance;
