d626481548afb10f0034c919d45de641bcd7dc36
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-26 15:09:34 +0530

    Commit

--- js/news.js
+++ js/news.js
@@ -21,11 +21,7 @@ class fetchdata {
          globalData.articles.map((value, index) =>{
            uniqueVal = `<h1>${value.author}</h1>`
            if(index > 0){
-              if(value.author === value.author){
-                uniqueVal = ""
-              }else{
-                uniqueVal = `<h1>${value.author}</h1>`
-              }
+                uniqueVal = "";
            }
           
             returnHtml += `${uniqueVal}<div class="newsTitle">${value.title}</div><div class="newsDescription">${value.description}</div><div class="publishDate">${value.publishedAt}</div><div class="imageContainer"><a href="${value.url}"><img src=${value.urlToImage} /></a></div>`;
