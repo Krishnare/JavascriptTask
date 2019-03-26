cde2eaea730beb56e34c5ca4648098e02caf3ca5
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-26 12:45:43 +0530

    Commit

--- js/news.js
+++ js/news.js
@@ -6,31 +6,23 @@ class fetchdata {
     constructor (data) {
         
     }
-
     SingletonOperation (url) {
         fetch(url)
         .then(function(response) {
           return response.json();
         })
         .then(function(myJson) {
-        //   console.log(JSON.stringify(myJson));
         globalData = myJson;
         let elementId = document.getElementById('newsDetails');
         let returnHtml = ""
         let duplicateResolve = ""
          globalData.articles.map((value, index) =>{
-           if(value.author !== value.author){
-            duplicateResolve = ""
-           }else{
-            duplicateResolve = value.author
-           }
-            returnHtml += `<h1>${duplicateResolve}</h1><div>${value.title}</div><div>${value.description}</div><div>${value.urlToImage}</div><div>${value.publishedAt}</div>`;
+            returnHtml += `<h1>${value.author}</h1><div>${value.title}</div><div>${value.description}</div><div class="imageContainer"><img src=${value.urlToImage} /></div><div>${value.publishedAt}</div>`;
         });
         console.log(returnHtml);
         elementId.insertAdjacentHTML('afterbegin', returnHtml);
         });
     }
-
     // GetSingletonData () {
         
     // }
