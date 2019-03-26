1a35b3862d3b637fece34942b2cbd7f70d177f0f
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-26 17:10:55 +0530

    Last commit

--- js/news.js
+++ js/news.js
@@ -24,7 +24,6 @@ class fetchdata {
           }
           returnHtml += `${uniqueVal}<div class="newsTitle">${value.title}</div><div class="newsDescription">${value.description}</div><div class="publishDate">${value.publishedAt}</div><div class="imageContainer"><a href="${value.url}" target="_blank"><img src=${value.urlToImage} /></a></div>`;
         });
-        console.log(returnHtml);
         elementId.insertAdjacentHTML('afterbegin', returnHtml);
       })
       .catch(error => { console.log("Something went wrong on server") });
