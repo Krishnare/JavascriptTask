ff1ed90feb2052ee23bfa0ea6033c8c2c5bdcdd4
Krishna Renabothu <Krishna_Renabothu@epam.com>
2019-03-28 13:54:36 +0530

    Changes Commit

--- js/apicall.js
+++ js/apicall.js
@@ -2,4 +2,6 @@ import instance from './news.js';
 const apiKey = "720c0314e8b2423eb7e1ffca5a1eeeb1";
 const newsCategory = "bbc-news";
 const url = `https://newsapi.org/v1/articles?source=${newsCategory}&apiKey=${apiKey}`;
-instance.fetchResults(url);
+const sourceUrl = "https://newsapi.org/v2/sources?apiKey=720c0314e8b2423eb7e1ffca5a1eeeb1";
+instance.apiFetcher(url);
+instance.apiSourceFetcher(sourceUrl);
--- js/news.js
+++ js/news.js
@@ -1,33 +1,52 @@
-
-'use strict';
+"use strict";
 class fetchdata {
-  // constructor(data) {
-  // }
-  fetchResults(url) {
-    let globalData = {};
+  apiFetcher(url) {
     fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(myJson => {
-        globalData = myJson;
-        instance.printResults(globalData);
-      })
-      .catch(error => { console.log("Something went wrong on server") });
+      .then(response => response.json())
+      .then(myJson => newsSource.articlesProvider(myJson))
+      .catch(error => console.log("Something went wrong on server"));
   }
-  printResults(data) {
-    let elementId = document.getElementById('newsDetails');
-    let returnHtml = "",
-      uniqueVal = [];
-    data.articles.map((value, index) => {
-      uniqueVal = `<h1>${value.author}</h1>`
-      if (index > 0) {
-        uniqueVal = "";
-      }
-      returnHtml += `${uniqueVal}<div class="newsTitle">${value.title}</div><div class="newsDescription">${value.description}</div><div class="publishDate">${value.publishedAt}</div><div class="imageContainer"><a href="${value.url}" target="_blank"><img src=${value.urlToImage} /></a></div>`;
-    });
-    elementId.insertAdjacentHTML('afterbegin', returnHtml);
+  apiSourceFetcher(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(myJson => newsSource.newsSourceChannel(myJson.sources))
+      .catch(error => console.log("News Source Channel || Something went wrong on server"));
   }
 }
+
+class newsSourceProvider extends fetchdata {
+  constructor(data) {
+    super(data);
+  }
+  articlesProvider(data) {
+    const elementId = document.getElementById("newsDetails");
+    let returnHtml = "",
+      uniqueVal = [];
+    data.articles.map((article, index) => {
+      uniqueVal = index === 0 ? `<h1>${article.author}</h1>` : "";
+      returnHtml += `${uniqueVal}<div class="newsTitle">${
+        article.title
+      }</div><div class="newsDescription">${
+        article.description
+      }</div><div class="publishDate">${
+        article.publishedAt
+      }</div><div class="imageContainer"><a href="${
+        article.url
+      }" target="_blank"><img src=${article.urlToImage} /></a></div>`;
+    });
+    elementId.insertAdjacentHTML("afterbegin", returnHtml);
+  }
+  newsSourceChannel(data){
+    const sourceSelectBox = document.getElementById('newsSource');
+   const selectOptions =  ""
+   data.map((source, index)=>{
+     console.log(source)
+      selectOptions = `<option>${source.id}</option>`
+    });
+    sourceSelectBox.insertAdjacentHTML("afterbegin", selectOptions);
+    console.log(data);
+  }
+}
+const newsSource = new newsSourceProvider();
 const instance = new fetchdata();
 export default instance;
